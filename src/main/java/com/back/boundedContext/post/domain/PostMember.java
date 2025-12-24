f7adf98e1f9abbd565a51fb76de50fe793707044
leehyeonwoo <dlgusdn7706@gmail.com>
2025-12-24 14:04:28 +0900

    0018

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -2,7 +2,6 @@ package com.back.boundedContext.post.domain;
 
 
 import com.back.shared.member.domain.ReplicaMember;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -10,12 +9,10 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "POST_MEMBER")
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
 public class PostMember extends ReplicaMember {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
 }
--- src/main/java/com/back/shared/member/domain/ReplicaMember.java
+++ src/main/java/com/back/shared/member/domain/ReplicaMember.java
@@ -1,19 +1,18 @@
 package com.back.shared.member.domain;
 
-import static lombok.AccessLevel.PROTECTED;
-
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @MappedSuperclass
 @Getter
-@Setter(value = PROTECTED)
+@Setter
 @NoArgsConstructor
-public abstract class ReplicaMember extends com.back.shared.member.domain.BaseMember {
+public abstract class ReplicaMember extends BaseMember {
     @Id
     private int id;
     private LocalDateTime createDate;
