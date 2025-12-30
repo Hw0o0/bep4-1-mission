6ea3c16e8d9531de4189f9233e5824de4f7d5e44
leehyeonwoo <dlgusdn7706@gmail.com>
2025-12-30 09:20:07 +0900

    0038

--- src/main/java/com/back/boundedContext/market/app/MarketCancelOrderRequestPaymentUseCase.java
+++ src/main/java/com/back/boundedContext/market/app/MarketCancelOrderRequestPaymentUseCase.java
@@ -2,17 +2,17 @@ package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.Order;
 import com.back.boundedContext.market.out.OrderRepository;
-import com.back.shared.cash.event.CashOrderPaymentFailedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MarketCancelOrderRequestPaymentUseCase {
+
     private final OrderRepository orderRepository;
 
-    public void handle(CashOrderPaymentFailedEvent event) {
-        Order order = orderRepository.findById(event.getOrder().getId()).get();
+    public void cancelRequestPayment(int orderId) {
+        Order order = orderRepository.findById(orderId).get();
 
         order.cancelRequestPayment();
     }
