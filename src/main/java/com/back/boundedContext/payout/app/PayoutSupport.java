f48247fd965039722cdf57a3c4ea5013ea9b5e19
leehyeonwoo <dlgusdn7706@gmail.com>
2025-12-30 15:18:26 +0900

    0045

--- src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
@@ -1,16 +1,23 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.PayoutCandidateItem;
 import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutCandidateItemRepository;
 import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PayoutSupport {
     private final PayoutMemberRepository payoutMemberRepository;
+    private final PayoutCandidateItemRepository payoutCandidateItemRepository;
+
+    public Optional<PayoutMember> findSystemMember() {
+        return payoutMemberRepository.findByUsername("system");
+    }
 
     public Optional<PayoutMember> findHolingMember() {
         return payoutMemberRepository.findByUsername("holding");
@@ -18,5 +25,9 @@ public class PayoutSupport {
 
     public Optional<PayoutMember> findMemberById(int id) {
         return payoutMemberRepository.findById(id);
+    }
+
+    public List<PayoutCandidateItem> findPayoutCandidateItems() {
+        return payoutCandidateItemRepository.findAll();
     }
 }
